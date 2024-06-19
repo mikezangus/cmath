ab4fd39c7274bd0a358a0d483fc0441e07201d5f
mikezangus <zangusm@gmail.com>
2024-06-19 15:48:10 -0400

    simplified convert logic per chatgpt

--- convert.c
+++ convert.c
@@ -1,16 +1,12 @@
 #include <ctype.h>
-#include <math.h>
 #include "header.h"
 
 void convert(void)
 {
-    int len1, len2;
-    for (len1 = 0; isdigit(eq.n1c[len1]); len1++)
-        ;;
-    for (len2 = 0; isdigit(eq.n2c[len2]); len2++)
-        ;;
-    for (int i = 0; isdigit(eq.n1c[i]); i++)
-        eq.n1d += (eq.n1c[i] - '0') * (1 * pow(10, len1 - i - 1));
-    for (int i = 0; isdigit(eq.n2c[i]); i++)
-        eq.n2d += (eq.n2c[i] - '0') * (1 * pow(10, len2 - i - 1));
+    eq.n1d = 0.0;
+    eq.n2d = 0.0;
+    for (int i = 0; i < eq.n1c_cnt; i++)
+        eq.n1d = eq.n1d * 10 + (eq.n1c[i] - '0');
+    for (int i = 0; i < eq.n2c_cnt; i++)
+        eq.n2d = eq.n2d * 10 + (eq.n2c[i] - '0');
 }
