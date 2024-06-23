b27c0a97e4f538e766b6694ba77e2011e8519379
mikezangus <zangusm@gmail.com>
2024-06-22 21:25:27 -0400

    added exponent operator

--- calculate.c
+++ calculate.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
+#include <math.h>
 #include "header.h"
 
 int calculate(void)
 {
+    printf("\n3. Calculate\n");
     eq.result = 0.0;
-    switch (eq.sign) {
+    switch (eq.oprtr) {
     case '+':
-        eq.result = eq.n1d + eq.n2d;
+        eq.result = eq.op1d + eq.op2d;
         break;
     case '-':
-        eq.result = eq.n1d - eq.n2d;
+        eq.result = eq.op1d - eq.op2d;
         break;
     case '*':
-        eq.result = eq.n1d * eq.n2d;
+        eq.result = eq.op1d * eq.op2d;
         break;
     case '/':
-        if (eq.n2d == 0) {
-            printf("Error: Unable to divide by 0\n");
+        if (eq.op2d == 0) {
+            printf("Error: Attempted divide by 0\n");
             return 0;
         } else {
-            eq.result = eq.n1d / eq.n2d;
+            eq.result = eq.op1d / eq.op2d;
             break;
         }
+    case '^':
+        eq.result = pow(eq.op1d, eq.op2d);
+        break;
     default:
-        printf("Error: Unable to calculate.\nSign: %c\n", eq.sign);
+        printf("Error: Unable to calculate.\nSign: %c\n", eq.oprtr);
         return 0;
     }
-    printf("Result: %f\n", eq.result);
-    return 0;
+    printf("Term result: %f\n----------------------------------------\n", eq.result);
+    return 1;
 }
--- parse.c
+++ parse.c
@@ -5,50 +5,48 @@
 
 void parse(int terms_idx)
 {
-    printf("ENTERING PARSE\n");
+    printf("\n----------------------------------------\nTERM %d\n", terms_idx + 1);
+    printf("\n1. Parse\n");
     for (int i = 0; i <= MAXLEN; i++) {
-        eq.n1c[i] = '\0';
+        eq.op1c[i] = '\0';
     }
     for (int i = 0; i <= MAXLEN; i++) {
-        eq.n2c[i] = '\0';
+        eq.op2c[i] = '\0';
     }
-    int n1c_cnt = 0;
-    int n2c_cnt = 0;
-    int sign_cnt = 0;
+    int op1c_cnt = 0;
+    int op2c_cnt = 0;
+    int oprtr_cnt = 0;
     char *term = terms[terms_idx];
     for (int i = 0; term[i] != '\0'; i++) {
         if (!terms_idx) {
-            printf("FIRST TERM\n");
-            if (term[i] == '+' || term[i] == '-' || term[i] == '*' || term[i] == '/') {
-                eq.sign = term[i];
-                sign_cnt++;
+            if (term[i] == '+' || term[i] == '-' || term[i] == '*' || term[i] == '/' || term[i] == '^') {
+                eq.oprtr = term[i];
+                oprtr_cnt++;
             } else if (isdigit(term[i])) {
-                if (!sign_cnt) {
-                    eq.n1c[n1c_cnt] = term[i];
-                    n1c_cnt++;
+                if (!oprtr_cnt) {
+                    eq.op1c[op1c_cnt] = term[i];
+                    op1c_cnt++;
                 } else {
-                    eq.n2c[n2c_cnt] = term[i];
-                    n2c_cnt++;
+                    eq.op2c[op2c_cnt] = term[i];
+                    op2c_cnt++;
                 }
             }
         } else {
-            printf("NOT FIRST TERM\n");
-            eq.n1c[n1c_cnt] = '0';
-            if (term[i] == '+' || term[i] == '-' || term[i] == '*' || term[i] == '/') {
-                eq.sign = term[i];
+            eq.op1c[op1c_cnt] = '0';
+            if (term[i] == '+' || term[i] == '-' || term[i] == '*' || term[i] == '/' || term[i] == '^') {
+                eq.oprtr = term[i];
             } else if (isdigit(term[i])) {
-                eq.n2c[n2c_cnt] = term[i];
-                n2c_cnt++;
+                eq.op2c[op2c_cnt] = term[i];
+                op2c_cnt++;
             }
         }
     }
-    printf("\nPARSE [%d]:\n", terms_idx);
-    printf("Sign: %c\n", eq.sign);
-    for (int i = 0; eq.n1c[i] != '\0'; i++) {
-        printf("n1c [%d] %c\n", i, eq.n1c[i]);
+    printf("Operator: %c\n", eq.oprtr);
+    for (int i = 0; eq.op1c[i] != '\0'; i++) {
+        printf("Operand 1 [%d]: %c\n", i, eq.op1c[i]);
     }
-    for (int i = 0; eq.n2c[i] != '\0'; i++) {
-        printf("n2c [%d] %c\n", i, eq.n2c[i]);
+    for (int i = 0; eq.op2c[i] != '\0'; i++) {
+        printf("Operand 2 [%d]: %c\n", i, eq.op2c[i]);
     }
     printf("Previous result: %f\n", eq.result);
 }
