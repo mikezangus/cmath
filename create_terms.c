c97cc4fbe0944471237ecb2ff5edeacfdb439ccf
mikezangus <zangusm@gmail.com>
2024-06-22 22:31:45 -0400

    efficiency

--- create_terms.c
+++ create_terms.c
@@ -10,7 +10,7 @@ int terms_idx = 0;
 char term[MAXLEN];
 int term_idx = 0;
 
-void init_term(void)
+void init_new_term(void)
 {
     term_idx = 0;
     for (int i = 0; i < MAXLEN; i++)
@@ -22,28 +22,24 @@ void copy_term_to_terms(void)
     for (int i = 0; term[i] != '\0'; i++) {
         printf("Copying '%c' term[%d] -> terms[%d]\n", term[i], i, terms_idx);
     }
-    strcpy(terms[terms_idx], term);
-    terms_idx++;
-    init_term();
+    strcpy(terms[terms_idx++], term);
+    init_new_term();
 }
 
 void create_terms(void)
 {
     int multi_term = 0;
-    init_term();
+    init_new_term();
     for (int i = 0; input[i] != '\0'; i++) {
         if (isdigit(input[i])) {
-            term[term_idx] = input[i];
-            term_idx++;
+            term[term_idx++] = input[i];
         } else if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/' || input[i] == '^') {
             if (!multi_term) {
-                term[term_idx] = input[i];
-                term_idx++;
+                term[term_idx++] = input[i];
                 multi_term++;
             } else {
                 copy_term_to_terms();
-                term[term_idx] = input[i];
-                term_idx++;
+                term[term_idx++] = input[i];
             }
         }
     }
--- parse.c
+++ parse.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 #include "header.h"
 
 
@@ -7,46 +8,33 @@ void parse(int terms_idx)
 {
     printf("\n----------------------------------------\nTERM %d\n", terms_idx + 1);
     printf("\n1. Parse\n");
-    for (int i = 0; i <= MAXLEN; i++) {
-        eq.op1c[i] = '\0';
-    }
-    for (int i = 0; i <= MAXLEN; i++) {
-        eq.op2c[i] = '\0';
-    }
+    memset(eq.op1c, '\0', MAXLEN);
+    memset(eq.op2c, '\0', MAXLEN);
     int op1c_cnt = 0;
     int op2c_cnt = 0;
     int oprtr_cnt = 0;
     char *term = terms[terms_idx];
     for (int i = 0; term[i] != '\0'; i++) {
-        if (!terms_idx) {
-            if (term[i] == '+' || term[i] == '-' || term[i] == '*' || term[i] == '/' || term[i] == '^') {
-                eq.oprtr = term[i];
-                oprtr_cnt++;
-            } else if (isdigit(term[i])) {
-                if (!oprtr_cnt) {
-                    eq.op1c[op1c_cnt] = term[i];
-                    op1c_cnt++;
-                } else {
-                    eq.op2c[op2c_cnt] = term[i];
-                    op2c_cnt++;
-                }
-            }
-        } else {
-            eq.op1c[op1c_cnt] = '0';
-            if (term[i] == '+' || term[i] == '-' || term[i] == '*' || term[i] == '/' || term[i] == '^') {
-                eq.oprtr = term[i];
-            } else if (isdigit(term[i])) {
-                eq.op2c[op2c_cnt] = term[i];
-                op2c_cnt++;
+        if (term[i] == '+' || term[i] == '-' || term[i] == '*' || term[i] == '/' || term[i] == '^') {
+            eq.oprtr = term[i];
+            oprtr_cnt++;
+        } else if (isdigit(term[i])) {
+            if (!oprtr_cnt) {
+                eq.op1c[op1c_cnt++] = term[i];
+            } else {
+                eq.op2c[op2c_cnt++] = term[i];
             }
         }
+        if (terms_idx) {
+            strcpy(eq.op1c, "0");
+        }
     }
-    printf("Operator: %c\n", eq.oprtr);
+    printf("   Operator: %c\n", eq.oprtr);
     for (int i = 0; eq.op1c[i] != '\0'; i++) {
-        printf("Operand 1 [%d]: %c\n", i, eq.op1c[i]);
+        printf("   Operand 1 [%d]: %c\n", i, eq.op1c[i]);
     }
     for (int i = 0; eq.op2c[i] != '\0'; i++) {
-        printf("Operand 2 [%d]: %c\n", i, eq.op2c[i]);
+        printf("   Operand 2 [%d]: %c\n", i, eq.op2c[i]);
     }
-    printf("Previous result: %f\n", eq.result);
+    printf("   Previous result: %f\n", eq.result);
 }
