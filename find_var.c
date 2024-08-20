8d94f9ab4d5507bb8a04158aa88c132809ebd648
mikezangus <zangusm@gmail.com>
2024-08-19 22:04:26 -0400

    better variables

--- find_var.c
+++ find_var.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 extern char input[];
-extern int var;
+extern bool var_exists;
+extern int i_var;
 
 void find_var(void)
 {
     for (int i = 0; input[i] != '\0'; i++) {
         if (input[i] == 'x') {
-            var = i;
+            var_exists = true;
+            i_var = i;
         }
     }
 }
