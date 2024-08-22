2adec81415299eb68670b30734b194ee29d92ef1
mikezangus <zangusm@gmail.com>
2024-08-21 22:38:00 -0400

    pointer arg

--- utils/print_str.c
+++ utils/print_str.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-void print_str(char m[], char s[])
+void print_str(char *m, char *s)
 {
     printf("\n%s\n", m);
     int len = strlen(s);
-    for (int i = 0; i <= len; i++) {
+    for (int i = 0; i < len; i++) {
         printf("[%d] %c\n", i, s[i]);
     }
 }
