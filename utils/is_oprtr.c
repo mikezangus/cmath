9143de4a7b0b16409bdfd700ee11df2368308966
mikezangus <zangusm@gmail.com>
2024-08-21 22:53:11 -0400

    is_oprtr in utils

---
+++ utils/is_oprtr.c
@@ -0,0 +1,16 @@
+#include "utils.h"
+
+int is_oprtr(char c)
+{
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+}
+
+int is_prec1_oprtr(char c)
+{
+    return (c == '^');
+}
+
+int is_prec2_oprtr(char c)
+{
+    return (c == '*' || c == '/');
+}
--- utils/utils.h
+++ utils/utils.h
@@ -1,3 +1,6 @@
 void collapse_str(char *, int, int);
 void expand_str(char *, int, int);
+int is_oprtr(char);
+int is_prec1_oprtr(char);
+int is_prec2_oprtr(char);
 void print_str(char *, char*);
