d99fd967a898ed5ef151175b3269f553d16f3489
mikezangus <zangusm@gmail.com>
2024-08-21 22:37:43 -0400

    pointer arg

--- utils/collapse_str.c
+++ utils/collapse_str.c
@@ -1,4 +1,4 @@
-void collapse_str(char s[], int start, int end)
+void collapse_str(char *s, int start, int end)
 {
     for (int i = start; s[i] != '\0'; i++) {
         s[i] = s[i + end - start + 1];
