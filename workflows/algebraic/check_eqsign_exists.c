f13efc4c0866f08c72bed2969eb790ddc16800ff
mikezangus <zangusm@gmail.com>
2024-08-31 14:56:27 -0400

    deletions

--- workflows/algebraic/check_eqsign_exists.c
+++
@@ -1,9 +0,0 @@
-// int check_eqsign_exists(char *s)
-// {
-//     for (char *p = s; *p != '\0'; p++) {
-//         if (*p == '=') {
-//             return 1;
-//         }
-//     }
-//     return 0;
-// }
--- workflows/algebraic/insert_eqsign.c
+++
@@ -1,9 +0,0 @@
-// #include <string.h>
-
-// void insert_eqsign(char *s)
-// {
-//     int len = strlen(s);
-//     s[len] = '=';
-//     s[len + 1] = '0';
-//     s[len + 2] = '\0';
-// }
