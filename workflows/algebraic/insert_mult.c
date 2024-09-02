3dedfd2c384bb9fb99d3f6068dab4c8281559ad0
mikezangus <zangusm@gmail.com>
2024-09-02 13:55:54 -0400

    cleaner

--- workflows/algebraic/insert_mult.c
+++ workflows/algebraic/insert_mult.c
@@ -1,17 +1,12 @@
-// #include "../../utils/utils.h"
-// #include <ctype.h>
+#include "../../utils/utils.h"
+#include <ctype.h>
 
-// void insert_mult(char *s)
-// {
-//     for (int i = 0; s[i] != '\0'; i++) {
-//         if (s[i] == 'x') {
-//             if (isdigit(s[i - 1])) {
-//                 insert_str(s, "*", i);
-//                 continue;
-//             }
-//             if (isdigit(s[i + 1])) {
-//                 insert_str(s, "*", i + 1);
-//             }
-//         }
-//     }
-// }
+void insert_mult(char *s)
+{
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == 'x' && isdigit(s[i - 1])) {
+            insert_str(s, "*", i);
+            i++;
+        }
+    }
+}
