4cc017da9896c4c0408caa223f7289e29fc8e0a6
mikezangus <zangusm@gmail.com>
2024-09-14 09:48:05 -0400

    simpler syntax, pointers instead of indexes, error handling

--- utils/insert_str.c
+++ utils/insert_str.c
@@ -1,26 +1,41 @@
+#include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 
-static void expand(char* destination_str, char* insertion_str, char* insert_position)
+static bool pos_is_inside_dest(char* dest_str, char* insert_pos)
 {
-    int a_len = strlen(destination_str);
-    int b_len = strlen(insertion_str);
-    int position_offset = insert_position - destination_str;
-    for (int i = a_len + b_len; i >= position_offset + b_len; i--) {
-        destination_str[i] = destination_str[i - b_len];
+    return (insert_pos >= dest_str) && (insert_pos < dest_str + strlen(dest_str));
+}
+
+static void expand(char* dest_str, int insert_len, char* insert_pos)
+{
+    int dest_len = strlen(dest_str);
+    char* copy = dest_str + dest_len;
+    char* paste = dest_str + dest_len + insert_len;
+    for (; copy >= insert_pos; copy--, paste--) {
+        *paste = *copy;
     }
 }
 
-static void insert(char* destination_str, char* insertion_str, char* insert_position) {
-    int j = 0;
-    int len = strlen(insertion_str);
-    int position_offset = insert_position - destination_str;
-    for (int i = position_offset; i < position_offset + len; i++) {
-        destination_str[i] = insertion_str[j++];
+static void insert(const char* insert_str, char* insert_pos)
+{
+    const char* copy = insert_str;
+    char* paste = insert_pos;
+    while (*copy) {
+        *paste++ = *copy++;
     }
 }
 
-void insert_str(char* destination_str, char* insertion_str, char* insert_position)
+void insert_str(char* dest_str, const char* insert_str, char* insert_pos)
 {
-    expand(destination_str, insertion_str, insert_position);
-    insert(destination_str, insertion_str, insert_position);
+    int insert_len = strlen(insert_str);
+    if (!pos_is_inside_dest(dest_str, insert_pos)) {
+        fprintf(
+            stderr,
+            "insert_str(): insert_pos not inside of dest_str\n  insert_str: %p - %p\n  insert_pos: %p\n",
+            insert_str, insert_str + insert_len, insert_pos);
+        return;
+    }
+    expand(dest_str, insert_len, insert_pos);
+    insert(insert_str, insert_pos);
 }
