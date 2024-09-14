1f3fa5cacf156b96d2ad6e448a666d6c6e58268b
mikezangus <zangusm@gmail.com>
2024-09-14 09:49:26 -0400

    updated function prototype

--- utils/utils.h
+++ utils/utils.h
@@ -5,7 +5,7 @@ bool char_is_var(char c);
 void collapse_str(char* s, int start, int end);
 void copy_bounded_str(char* input_start, char* input_end, char* output);
 
-void insert_str(char* destination_str, char* insertion_str, char* insert_position);
+void insert_str(char* dest_str, const char* insert_str, char* insert_pos);
 
 bool is_oprtr(char c);
 bool is_prec1_oprtr(char c);
