bebf029799b6119017ef847ac3a46940e7bebc5e
mikezangus <zangusm@gmail.com>
2024-09-28 23:40:33 -0400

    conversions return conversions

--- workflows/algebraic/calculate_vars.c
+++ workflows/algebraic/calculate_vars.c
@@ -13,14 +13,14 @@ static void process_prec2(struct EquationAlg* eq)
             exp_oprtr = '-';
             break;
     }
-    calculate_result(eq->op1_num_base_d, eq->oprtr, eq->op2_num_base_d, &eq->res_num_base_d);
+    eq->res_num_base_d = calculate_result(eq->op1_num_base_d, eq->oprtr, eq->op2_num_base_d);
     eq->res_num_var = eq->op1_num_var;
-    calculate_result(eq->op1_num_exp_d, exp_oprtr, eq->op2_num_exp_d, &eq->res_num_exp_d);
+    eq->res_num_exp_d = calculate_result(eq->op1_num_exp_d, exp_oprtr, eq->op2_num_exp_d);
 }
 
 static void process_prec3(struct EquationAlg* eq)
 {
-    calculate_result(eq->op1_num_base_d, eq->oprtr, eq->op2_num_base_d, &eq->res_num_base_d);
+    eq->res_num_base_d = calculate_result(eq->op1_num_base_d, eq->oprtr, eq->op2_num_base_d);
     eq->res_num_var = eq->op1_num_var;
     eq->res_num_exp_d = eq->op1_num_exp_d;
 }
