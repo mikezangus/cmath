a44af1380f96dfacd9dd12730ca5385e60ec0130
mikezangus <zangusm@gmail.com>
2024-09-30 23:05:23 -0400

    replaced custom expand and insert functions with memmove and memcpy

--- utils/insert_str.c
+++ utils/insert_str.c
@@ -7,33 +7,20 @@ static bool is_pos_inside_dst(char* dst_str, size_t dst_len, const char* insert_
     return (insert_pos >= dst_str) && (insert_pos <= dst_str + dst_len);
 }
 
-static void expand(char* dst_str, size_t dst_len, const char* insert_pos, size_t insert_len)
-{
-    const char* src = dst_str + dst_len;
-    char* dst = dst_str + dst_len + insert_len;
-    while (src >= insert_pos) {
-        *dst-- = *src--;
-    }
-}
-
-static void insert(const char* src_str, const char* insert_pos)
-{
-    char* dst = (char*)insert_pos;
-    while (*src_str) {
-        *dst++ = *src_str++;
-    }
-}
-
 void insert_str(char* dst_str, const char* src_str, const char* insert_pos)
 {
     size_t dst_len = strlen(dst_str);
     if (!is_pos_inside_dst(dst_str, dst_len, insert_pos)) {
         fprintf(
             stderr,
-            "\n%s | Error: insert_pos not inside of dst_str\n  dst_str: %p - %p\n  insert_pos: %p\n",
-            __FILE__, dst_str, dst_str + dst_len, insert_pos);
+            "\n%s | Error: insert_pos not inside of dst_str\n"
+            "  dst_str: %p - %p\n"
+            "  insert_pos: %p\n",
+            __FILE__, dst_str, dst_str + dst_len, insert_pos
+        );
         return;
     }
-    expand(dst_str, dst_len, insert_pos, strlen(src_str));
-    insert(src_str, insert_pos);
+    size_t src_len = strlen(src_str);
+    memmove((char*)insert_pos + src_len, insert_pos, dst_len - (insert_pos - dst_str) + 1);
+    memcpy((char*)insert_pos, src_str, src_len);
 }
