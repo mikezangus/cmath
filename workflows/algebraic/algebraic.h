90e3db0f75fa30ba9e9d62232b3fe8b0abfd6d4d
mikezangus <zangusm@gmail.com>
2024-10-01 00:19:38 -0400

    created insert exp 1

--- workflows/algebraic/algebraic.h
+++ workflows/algebraic/algebraic.h
@@ -2,48 +2,7 @@
 #include <stdbool.h>
 
 
-void create_var_str(char* in_var, char* in_exp, char* out);
-char* find_var(char* s);
-void init_vars(void);
-void insert_1(char* s);
-void insert_eqzero(char* s);
-bool is_var(char c);
-void parse_var(char* in, char* out_var, double* out_exp);
-void process_algebraic(char* s);
-void rearrange(char* s);
-void remove_1(char* s);
-void remove_exp1(char* s);
-void remove_vars(char* s);
-void solve_alg(char* s);
-int var_exists(char* s);
-
-struct Variables {
-    char op1n_var;
-    double op1n_exp;
-    char op1d_var;
-    double op1d_exp;
-
-    char op2n_var;
-    double op2n_exp;
-    char op2d_var;
-    double op2d_exp;
-
-    char result_varn[STR_MAXLEN];
-    double result_expnd;
-    char result_expns[STR_MAXLEN];
-
-    char result_vard[STR_MAXLEN];
-    double result_expdd;
-    char result_expds[STR_MAXLEN];
-
-    char resultns[STR_MAXLEN];
-    char resultds[STR_MAXLEN];
-};
-extern struct Variables vars;
-
-
-
-struct EquationAlg {
+struct EqAlg {
 
     char oprtr;
 
@@ -100,7 +59,22 @@ struct EquationAlg {
     char result[STR_MAXLEN];
     
 };
-extern struct EquationAlg eq_alg;
+extern struct EqAlg eq_alg;
 
 
-void calculate_vars(struct EquationAlg* eq);
+void calculate_vars(struct EqAlg* eq_alg);
+void create_var_str(char* in_var, char* in_exp, char* out);
+char* find_var(char* s);
+void init_vars(void);
+void insert_eqzero(char* s);
+void insert_exp1(char* s);
+void insert_mult1(char* s);
+bool is_var(char c);
+void parse_var(char* in, char* out_var, double* out_exp);
+void process_algebraic(char* s);
+void rearrange(char* s);
+void remove_mult1(char* s);
+void remove_exp1(char* s);
+void remove_vars(char* s);
+void solve_alg(char* s);
+int var_exists(char* s);
