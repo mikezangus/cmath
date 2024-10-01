1b27e4afe459156f3bcc04589f929a8f4e096d89
mikezangus <zangusm@gmail.com>
2024-09-30 21:45:52 -0400

    new eq struct

--- workflows/arithmetic/process_arithmetic.c
+++ workflows/arithmetic/process_arithmetic.c
@@ -1,13 +1,25 @@
+#include <string.h>
+#include "./arithmetic.h"
 #include "../../general/general.h"
 #include "../../general/verifications/verifications.h"
-#include "./arithmetic.h"
+
+struct EqAr eq;
+
+static void init(struct EqAr* eq)
+{
+    memset(eq->op1_num_s, '\0', STR_MAXLEN);
+    memset(eq->op1_den_s, '\0', STR_MAXLEN);
+    eq->oprtr = '\0';
+    memset(eq->op2_num_s, '\0', STR_MAXLEN);
+    memset(eq->op2_den_s, '\0', STR_MAXLEN);
+}
 
 void process_arithmetic(char* s)
 {
-    insert_parens(s);
-    // while (verify(s)) {
-    for (int i = 0; i < 2; i++) {
-        init_eq();
-        solve(s);
+    while (verify(s)) {
+    // for (int i = 0; i < 2; i++) {
+        insert_parens(s);
+        init(&eq);
+        solve(s, &eq);
     }
 }
