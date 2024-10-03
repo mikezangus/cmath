0b6bdfa8675c18439a891e285800051bc6a2dee3
mikezangus <zangusm@gmail.com>
2024-10-02 23:07:35 -0400

    compatible with leadng negative

--- workflows/arithmetic/parsing/parse.c
+++ workflows/arithmetic/parsing/parse.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include "../arithmetic.h"
-#include "../bounding/bounding.h"
-#include "../../../utils/utils.h"
-#include "../../../general/general.h"
 #include "parsing.h"
+#include "../arithmetic.h"
+#include "../../../general/general.h"
+#include "../../../general/bounding/bounding.h"
+#include "../../../utils/utils.h"
 
 
 static void parse_operation(char** start, char** end, struct EqAr* eq)
 {
-    char* oprtr = find_oprtr(*start, *end);
+    char* oprtr = find_oprtr((*start) + 1, *end);
     if (*oprtr == '/') {
         parse_division(start, oprtr, end, eq);
     } else {
@@ -23,15 +23,17 @@ static void parse_operation(char** start, char** end, struct EqAr* eq)
 
 void parse_arithmetic(char* s, struct EqAr* eq, char** l_bound, char** r_bound)
 {
-    if (!find_bounds_ar(s, l_bound, r_bound)) {
+    if (!find_bounds(s, l_bound, r_bound)) {
         return;
     }
     parse_operation(l_bound, r_bound, eq);
-
-    printf("\nPARSED:\n");
-    printf("  Op1 num: %s\n", eq->op1_num_s);
-    printf("  Op1 den: %s\n", eq->op1_den_s);
-    printf("  Oprtr:   %c\n", eq->oprtr);
-    printf("  Op2 num: %s\n", eq->op2_num_s);
-    printf("  Op2 den: %s\n\n", eq->op2_den_s);
+    printf(
+        "\nParsed:\n"
+        "  Op1 num: %s\n"
+        "  Op1 den: %s\n"
+        "  Oprtr:   %c\n"
+        "  Op2 num: %s\n"
+        "  Op2 den: %s\n\n",
+        eq->op1_num_s, eq->op1_den_s, eq->oprtr, eq->op2_num_s, eq->op2_den_s
+    );
 }
