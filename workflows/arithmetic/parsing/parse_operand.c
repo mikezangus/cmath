4e4904ae7b5982e359795b39a30cd15e7cda165e
mikezangus <zangusm@gmail.com>
2024-10-07 23:47:11 -0400

    formatting

--- workflows/arithmetic/parsing/parse_operand.c
+++ workflows/arithmetic/parsing/parse_operand.c
@@ -1,8 +1,8 @@
 #include <ctype.h>
 
-void parse_operand(const char* l_bound, const char* r_bound, char* dst)
+void parse_operand(char* dst, const char* start, const char* end)
 {
-    for (const char* p = l_bound; p <= r_bound; p++) {
+    for (const char* p = start; p <= end; p++) {
         if (isdigit(*p) || *p == '-') {
             *dst++ = *p;
         }
