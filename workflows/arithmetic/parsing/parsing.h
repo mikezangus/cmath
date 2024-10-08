8fe2f5c3ddc998fdbf8f5ef29f582359c52de844
mikezangus <zangusm@gmail.com>
2024-10-07 23:47:46 -0400

    custom return type for parsing

--- workflows/arithmetic/parsing/parsing.h
+++ workflows/arithmetic/parsing/parsing.h
@@ -1,6 +1,13 @@
 #include <stdbool.h>
 #include "../arithmetic.h"
 
-void parse_arithmetic(char* s, EqAr* eq, char** l_bound, char** r_bound);
-bool parse_division(char** start, char* div_op, char** end, EqAr* eq);
-void parse_operand(const char* l_bound, const char* r_bound, char* dst);
+typedef enum  {
+    FAILED_TO_PARSE = 0,
+    PARSED_TO_CURRENT = 1,
+    PARSED_TO_NEW = 2,
+    FIND_NEW_OPERATION = 3,
+} Status;
+
+void parse_arithmetic(char* s, EqAr* eq, Bounds* b);
+bool parse_division(char* s, char* div_oprtr, EqAr* eq, Bounds* b);
+void parse_operand(char* dst, const char* start, const char* end);
