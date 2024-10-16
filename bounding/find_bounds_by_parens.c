6437661360eabca11d1d3e675834d600e107f5c9
mikezangus <zangusm@gmail.com>
2024-10-16 02:06:12 -0400

    broke find paren set into own function

--- bounding/find_bounds_by_parens.c
+++ bounding/find_bounds_by_parens.c
@@ -1,20 +1,12 @@
 #include <stdbool.h>
 #include <stdio.h>
-#include "../main.h"
+#include "../utils/utils.h"
 
 bool find_bounds_by_parens(char* start, char** l_bound, char** r_bound)
 {
     char* l_paren = NULL;
     char* r_paren = NULL;
-    for (char* p = start; *p && *p != '='; p++) {
-        if (*p == '(') {
-            l_paren = p;
-        } else if (l_paren && *p == ')') {
-            r_paren = p;
-            break;
-        }
-    }
-    if (!l_paren || !r_paren) {
+    if (!find_paren_set(start, &l_paren, &r_paren)) {
         return false;
     }
     *l_bound = l_paren + 1;
