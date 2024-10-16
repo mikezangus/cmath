06367acee278ca4d05263f12e0c9ab439f97b2b5
mikezangus <zangusm@gmail.com>
2024-10-16 02:03:33 -0400

    created

---
+++ formatting/convert_neg_signs.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "formatting.h"
+
+void convert_neg_signs(char* s)
+{
+    char* p = s + 1;
+    while (*p) {
+        char* oprtr = strchr(p, '-');
+        if (!oprtr) {
+            return;
+        }
+        char* prev_oprtr = oprtr - 1;
+        if (*prev_oprtr == '-') {
+            *oprtr = '+';
+        }
+        if (*prev_oprtr == '+' || *prev_oprtr == '-') {
+            collapse_str(prev_oprtr, prev_oprtr);
+            p = oprtr;
+        } else {
+            p = oprtr + 1;
+        }
+    }
+}
---
+++ formatting/formatting.h
@@ -0,0 +1,8 @@
+#include "../utils/utils.h"
+
+void add_parens(char* s);
+void format_parens(char* s);
+void remove_parens(char* s);
+
+void convert_neg_signs(char* s);
+void move_fract_neg_sign(char* s);
