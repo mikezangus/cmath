8b5475f41365ad954304c9d79eb9ba2ddb892f1b
mikezangus <zangusm@gmail.com>
2024-10-22 23:16:00 -0400

    less fluff

--- utils/searches/find_nearest_oprtr.c
+++ utils/searches/find_nearest_oprtr.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include "../utils.h"
 
-static char* find_l(char* s, char* l_start)
+static char* find_l(const char* s, const char* l_start)
 {
-    for (char* p = l_start; p > s && *p != '(' && *p != ')'; p--) {
-        if (is_oprtr(*p)) {
-            return p;
-        }
-    }
-    return NULL;
-}
-
-static char* find_r(char* r_start)
-{
-    for (char* p = r_start; *p && *p != '(' && *p != ')'; p++) {
-        if (is_oprtr(*p)) {
-            return p;
-        }
-    }
-    return NULL;
-}
-
-char* find_nearest_oprtr(char* s, char* l_start, char* r_start)
-{
-    l_start && is_paren(*l_start) && l_start != s
-        ? l_start--
+    const char* p = is_paren(*l_start) && l_start > s
+        ? l_start - 1
         : l_start;
-    r_start && is_paren(*r_start)
-        ? r_start++
+    for (; p > s && !is_paren(*p); p--) {
+        if (is_oprtr(*p)) {
+            return (char*)p;
+        }
+    }
+    return NULL;
+}
+
+static char* find_r(const char* r_start)
+{
+    const char* p = is_paren(*r_start)
+        ? r_start + 1
         : r_start;
+    for (; *p && !is_paren(*p); p++) {
+        if (is_oprtr(*p)) {
+            return (char*)p;
+        }
+    }
+    return NULL;
+}
+
+char* find_nearest_oprtr(const char* s,
+                         const char* l_start, const char* r_start)
+{
+
     char* l = l_start
         ? find_l(s, l_start)
         : NULL;
