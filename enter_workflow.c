7f3c6c97abd7a1c5bc6e96e0c016f6c7bbf626b6
mikezangus <zangusm@gmail.com>
2024-10-25 20:43:38 -0400

    no longer running verify for the while loop, just exiting whenever an inner function returns

--- enter_workflow.c
+++ enter_workflow.c
@@ -1,9 +1,10 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 #include "bounding/bounding.h"
 #include "formatting/formatting.h"
-#include "verifications/verifications.h"
+#include "general/general.h"
 #include "utils/utils.h"
 #include "workflows/algebraic/algebraic.h"
 #include "workflows/arithmetic/arithmetic.h"
@@ -12,11 +13,12 @@ Bounds b;
 
 void enter_workflow(char* s)
 {
-    while (verify(s)) {
-    // for (int i = 0; i < 6; i++) {
-        format_parens(s);
-        convert_neg_signs(s);
-        printf("%s\nStarting: %s\n", DASHES, s);
+    while (true) {
+        if (!is_paren_depth_same(s, s + strlen(s))) {
+            return;
+        }
+        format_str(s);
+        printf("\nStarting: %s\n", s);
         if (!find_bounds(s, &b.l, &b.r)) {
             return;
         }
@@ -27,5 +29,4 @@ void enter_workflow(char* s)
             // process_algebraic(s);
         }
     }
-    format_parens(s);
 }
