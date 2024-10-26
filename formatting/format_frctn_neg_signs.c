794ca674b7f43fbadae8de51be2107ddecdff1ab
mikezangus <zangusm@gmail.com>
2024-10-25 23:31:55 -0400

    moved and deleted

---
+++ formatting/format_frctn_neg_signs.c
@@ -0,0 +1,52 @@
+#include <ctype.h>
+#include "../main.h"
+#include "../utils/utils.h"
+
+static void remove_double_neg_signs(char* s, char* div_oprtr, Bounds* b)
+{
+    if (*(b->l) != '-' || *(div_oprtr + 1) != '-') {
+        return;
+    }
+    collapse_str(b->l, b->l);
+    collapse_str(div_oprtr, div_oprtr);
+    b->r-=2;
+}
+
+static void move_to_numerator(char* s, char** div_oprtr, Bounds* b)
+{
+    char* neg_sign = *div_oprtr + 1;
+    if (*neg_sign != '-') {
+        return;
+    }
+    collapse_str(neg_sign, neg_sign);
+    insert_str(s, "-", b->l);
+    (*div_oprtr)++;
+}
+
+static void move_to_inside_parens(char* s, Bounds* b)
+{
+    if (b->l - 2 < s) {
+        return;
+    }
+    char* neg_sign = b->l - 2;
+    if (*neg_sign != '-'
+        || *(neg_sign + 1) != '('
+        || neg_sign + 1 != b->l - 1
+        || neg_sign > s && isdigit(*(neg_sign - 1))) {
+        return;
+    }
+    b->l--;
+    collapse_str(neg_sign, neg_sign);
+    insert_str(s, "-", b->l);
+}
+
+void format_frctn_neg_signs(char* s, Bounds* b)
+{
+    char* div_oprtr = find_char('/', b->l + 1, b->r - 1);
+    if (!div_oprtr) {
+        return;
+    }
+    remove_double_neg_signs(s, div_oprtr, b);
+    move_to_numerator(s, &div_oprtr, b);
+    move_to_inside_parens(s, b);
+}
--- formatting/formatting.h
+++ formatting/formatting.h
@@ -1,14 +1,11 @@
+#include <stdio.h>
 #include "../main.h"
 #include "../utils/utils.h"
 
-void move_frctn_neg_sign(char* s, Bounds* b);
-
-void flip_frctn_divisor(char* oprtr, char* num, char* den);
-bool format_frctn_oprtn(char* n1, char* d1, char* oprtr, char* n2, char* d2);
-void format_neg_frctn(char* num, char* den);
+void flip_frctn_divisor(char* oprtr, char* n, char* d);
+void format_frctn_neg_signs(char* s, Bounds* b);
 
 void convert_neg_signs(char* s);
 void format_str(char* s);
 void insert_parens(char* s);
 void remove_unneeded_parens(char* s);
-
