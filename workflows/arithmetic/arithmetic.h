cb01b850f8b2c58d37f2023527d2b5c257203b22
mikezangus <zangusm@gmail.com>
2024-10-25 23:40:06 -0400

    moved parsing header to arithmetic header

--- workflows/arithmetic/arithmetic.h
+++ workflows/arithmetic/arithmetic.h
@@ -31,6 +31,22 @@ typedef struct OprtnAr {
 } OprtnAr;
 
 
+typedef enum  {
+    FAIL = 0,
+    PARSED_L_TO_PREV = 1,
+    PARSED_R_TO_PREV = 2,
+    PARSED_TO_NEW = 3,
+} DivStatus;
+
+
+char* extract_num_bwd(char* dst, const char* start, const char* min);
+char* extract_num_fwd(char* dst, const char* start);
+bool parse_arithmetic(const char* s, const char* start, bool bounds_found,
+                      OprtnAr* o, Bounds* b);
+bool parse_inoperable_division(const char* s, const char* op1, const char* op2,
+                               OprtnAr* o, Bounds* b);
+void parse_oprtn(char* dst_op1, char* dst_oprtr, char* dst_op2,
+                 const char* src_op1, char src_oprtr, const char* src_op2);
 bool solve_arithmetic(char* s, Bounds* b);
 
 
