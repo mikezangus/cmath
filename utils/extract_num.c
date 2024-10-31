a395c3c26a755b54537b93016bcd161d577253cf
mikezangus <zangusm@gmail.com>
2024-10-30 23:53:19 -0400

    loop

--- utils/extract_num.c
+++ utils/extract_num.c
@@ -5,8 +5,8 @@
 char* extract_num_fwd(char* dst, const char* start)
 {
     const char* p = start;
-    for (; *p && is_num(p, start); p++) {
-        *dst++ = *p;
+    while (*p && is_num(p, start)) {
+        *dst++ = *p++;
     }
     *dst = '\0';
     return (char*)(p - 1);
@@ -16,6 +16,7 @@ char* extract_num_bwd(char* dst, const char* start, const char* min)
 {
     const char* p = start;
     for (; p > min && is_num(p, NULL); p--) {
+        ;;
     }
     p = (p == min && (isdigit(*p) || *min == '-')) ? p : p + 1;
     for (const char* q = p; q <= start; q++) {
