419ac288c0992ea21d9aab1900179f8372214ef5
mikezangus <zangusm@gmail.com>
2024-11-01 16:59:14 -0400

    simpler

--- workflows/algebraic/isolation/isolate_num_oprtn.c
+++ workflows/algebraic/isolation/isolate_num_oprtn.c
@@ -9,6 +9,10 @@ static void extract_num_oprtn(char* dst,
                               char** start, char** end,
                               const char* src)
 {
+    char* eq_sign = strchr(src, '=');
+    if (!eq_sign) {
+        return;
+    }
     *start = *end = NULL;
     char* p_dst = dst;
     if (isdigit(*src)) {
@@ -17,11 +21,8 @@ static void extract_num_oprtn(char* dst,
         *end = extract_num_fwd(p_dst, src);
         return;
     }
-    for (const char* p_src = src, * eq_sign, * oprtr;
-         *p_src && *p_src != '=';
-         p_src = oprtr + 1) {
-        if (!(eq_sign = strchr(p_src, '='))
-            || !(oprtr = find_oprtr(p_src, eq_sign - 1))) {
+    for (const char* p_src = src, * oprtr; p_src < eq_sign; p_src = oprtr + 1) {
+        if (!(oprtr = find_oprtr(p_src, eq_sign - 1))) {
             break;
         }
         if (get_paren_depth(oprtr, eq_sign - 1) != 0) {
@@ -42,7 +43,6 @@ bool isolate_num_oprtn(char* s)
     if (*oprtn == '\0' || !start || !end) {
         return false;
     }
-    printf("Extraction: %s\n", oprtn);
     collapse_str(start, end);
     insert_str(s, oprtn, s + strlen(s));
     return true;
