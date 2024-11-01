753afe8cf2c7f572e912da8ec104405c9b1477ec
mikezangus <zangusm@gmail.com>
2024-11-01 15:48:03 -0400

    isolate_num_oprtn improved and in own file, handle src[0] == '+' after extract

---
+++ workflows/algebraic/isolation/isolate_num_oprtn.c
@@ -0,0 +1,49 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include "../../../main.h"
+#include "../../../utils/utils.h"
+
+static void extract_num_oprtn(char* dst,
+                              char** start, char** end,
+                              const char* src)
+{
+    *start = *end = NULL;
+    char* p_dst = dst;
+    if (isdigit(*src)) {
+        *p_dst++ = '-';
+        *start = (char*)src;
+        *end = extract_num_fwd(p_dst, src);
+        return;
+    }
+    for (const char* p_src = src, * eq_sign, * oprtr;
+         *p_src && *p_src != '=';
+         p_src = oprtr + 1) {
+        if (!(eq_sign = strchr(p_src, '='))
+            || !(oprtr = find_oprtr(p_src, eq_sign - 1))) {
+            break;
+        }
+        if (get_paren_depth(oprtr, eq_sign - 1) != 0) {
+            continue;
+        }
+        *p_dst++ = invert_oprtr(*oprtr);
+        *start = (char*)oprtr;
+        *end = extract_num_fwd(p_dst, oprtr + 1);
+        break;
+    }
+}
+
+bool isolate_num_oprtn(char* s)
+{
+    char oprtn[STR_MAXLEN] = {0};
+    char* start = NULL, * end = NULL;
+    extract_num_oprtn(oprtn, &start, &end, s);
+    if (*oprtn == '\0' || !start || !end) {
+        return false;
+    }
+    printf("Extraction: %s\n", oprtn);
+    collapse_str(start, end);
+    insert_str(s, oprtn, s + strlen(s));
+    return true;
+}
