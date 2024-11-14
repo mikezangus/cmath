adad2d67543644461e85c4d9e7f62f5944ce987e
mikezangus <zangusm@gmail.com>
2024-11-13 23:26:17 -0500

    formatting

--- utils/searches/find_adj_oprtr.c
+++ utils/searches/find_adj_oprtr.c
@@ -25,7 +25,6 @@ static char* find_r(const char* r_start)
 
 char* find_adj_oprtr(const char* s, const char* l_start, const char* r_start)
 {
-
     char* l = l_start ? find_l(s, l_start) : NULL;
     char* r = r_start ? find_r(r_start) : NULL;
     if (!l && !r) {
