0885d942b750d0a9b12c7cf324fcb31b73feb304
mikezangus <zangusm@gmail.com>
2024-11-13 23:26:11 -0500

    root handling that works

--- general/calculate_oprtn.c
+++ general/calculate_oprtn.c
@@ -12,10 +12,6 @@ double calculate_oprtn(double op1, char oprtr, double op2)
         case '*': return op1 * op2;
         case '/': return (op2 == 0.0) ? NAN : op1 / op2;
         case '^': return pow(op1, op2);
-        case '√': 
-            return (op2 == 0 || (op1 < 0 && ((int)op2 % 2 == 0)))
-                ? NAN
-                : pow(op1, 1.0 / op2);
         default:
             fprintf(stderr, "\n%s | Invalid operator: %c\n", __FILE__, oprtr);
             return NAN;
