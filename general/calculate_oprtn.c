224a33fa5b7b70549bcab27b40aa4de061c69e48
mikezangus <zangusm@gmail.com>
2024-11-18 21:06:42 -0500

    root support

--- general/calculate_oprtn.c
+++ general/calculate_oprtn.c
@@ -1,17 +1,37 @@
 #include <math.h>
 #include <stdio.h>
 
-double calculate_oprtn(double op1, char oprtr, double op2)
+static double calculate_root_oprtn(double base, int expn, int expd)
 {
-    if (isnan(op1) || isnan(op2)) {
+    if (expd == 0) {
+        fprintf(stderr,
+                "%s\nError | Zero denominator in root calculation: %f^%d/%d\n",
+                __FILE__, base, expn, expd);
+        return NAN;
+    }
+    if (base < 0 && expd % 2 == 0) {
+        fprintf(stderr,
+                "%s\nError | Negative base with even root: %f^%d/%d\n",
+                __FILE__, base, expn, expd);
+        return NAN;
+    }
+    double root = pow(base, 1.0 / expd);
+    return pow(root, expn);
+}
+
+double calculate_oprtn(double op1, char oprtr, double op2n, double op2d)
+{
+    if (isnan(op1) || isnan(op2n)) {
         return NAN;
     }
     switch (oprtr) {
-        case '+': return op1 + op2;
-        case '-': return op1 - op2;
-        case '*': return op1 * op2;
-        case '/': return (op2 == 0.0) ? NAN : op1 / op2;
-        case '^': return pow(op1, op2);
+        case '+': return op1 + op2n;
+        case '-': return op1 - op2n;
+        case '*': return op1 * op2n;
+        case '/': return (op2n == 0.0) ? NAN : op1 / op2n;
+        case '^': return op2d
+            ? calculate_root_oprtn(op1, op2n, op2d)
+            : pow(op1, op2n);
         default:
             fprintf(stderr, "\n%s | Invalid operator: %c\n", __FILE__, oprtr);
             return NAN;
--- general/general.h
+++ general/general.h
@@ -2,7 +2,7 @@
 
 void appy_exp_rule_to_frctns(char* s);
 double calculate_den(double* n1, double* d1, char oprtr, double* n2, double* d2);
-double calculate_oprtn(double op1, char oprtr, double op2);
+double calculate_oprtn(double op1, char oprtr, double op2n, double op2d);
 void create_result_str(char* dst, char* n, char* d);
 void equate_denoms(double* p_n1, double* p_d1, double* p_n2, double* p_d2);
 bool is_str_operable(char* s);
