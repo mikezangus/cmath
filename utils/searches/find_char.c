c8cf79c37164f63f3c0bfe57cd6739204b050cd3
mikezangus <zangusm@gmail.com>
2024-11-25 15:24:36 -0700

    works with null end

--- utils/searches/find_char.c
+++ utils/searches/find_char.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 char* find_char(char c, const char* start, const char* end)
 {
+    if (!end) {
+        end = start + strlen(start);
+    }
     if (start > end) {
         return NULL;
     }
-    for (const char* p = start; *p && p <= end; p++) {
+    for (const char* p = start; p <= end; p++) {
         if (*p == c) {
             return (char*)p;
         }
