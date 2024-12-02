865511f11899c0ab1233169767460867dc20eaf0
mikezangus <zangusm@gmail.com>
2024-12-01 23:49:42 -0500

    works with null p2

--- utils/get_paren_depth.c
+++ utils/get_paren_depth.c
@@ -1,9 +1,11 @@
+#include <string.h>
 #include "utils.h"
 
 int get_paren_depth(const char* p1, const char* p2)
 {
     int scale = 0;
-    for (const char* p = p1; p <= p2; p++) {
+    const char* end = p2 ? p2 : p1 + strlen(p1);
+    for (const char* p = p1; p <= end; p++) {
         balance_chars(*p, &scale, '(', ')');
     }
     return scale;
