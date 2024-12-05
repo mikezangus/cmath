f6b37495ee51534b4a335513223968ef7164cd08
mikezangus <zangusm@gmail.com>
2024-12-04 23:18:17 -0500

    better quotient rule handling

--- general/general.h
+++ general/general.h
@@ -1,9 +1,11 @@
 #include <stdbool.h>
 
-void appy_exp_rule_to_frctns(char* s);
+void apply_quotient_rule(char* s);
 double calculate_den(double* n1, double* d1, char oprtr, double* n2, double* d2);
 double calculate_oprtn(double op1, char oprtr, double op2n, double op2d);
 void create_result_str(char* dst, char* n, char* d);
 void equate_denoms(double* p_n1, double* p_d1, double* p_n2, double* p_d2);
+void exp_rules(char* s);
 bool is_str_operable(char* s);
+void rationalize_denominator(char* src);
 void reduce_frctn(double* n, double* d);
---
+++ general/pow_rules/apply_quotient_rule.c
@@ -0,0 +1,65 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include "../../main.h"
+#include "../../utils/utils.h"
+
+static char* find_div_sign(const char* start, const char* min)
+{
+    char* den_start = extract_num_bwd((char[]){0}, start, min);
+    if (den_start - 1 < min || *(den_start - 1) != '/') {
+        return NULL;
+    }
+    return den_start - 1;
+}
+
+static char* extract_exp(char* dst, char* src)
+{
+    bool parens = false;
+    char* p = src;
+    while (*p && *p != ')') {
+        if (*p == '(') {
+            parens = true;
+        }
+        *dst++ = *p++;
+    }
+    if (parens) {
+        *dst++ = *p++;
+    }
+    *dst++ = ')';
+    *dst = '\0';
+    collapse_str(src, p);
+    return (*p && p > src) ? (char*)(p - 1) : NULL;
+}
+
+void apply_quotient_rule(char* s)
+{
+    char* p = s;
+    char* pow_sign;
+    char* div_sign;
+    char exp[STR_MAXLEN] = {0};
+    size_t exp_len;
+    while (*p) {
+        if (!(pow_sign = strchr(p, '^'))) {
+            return;
+        }
+        if (pow_sign - 6 < s
+            || *(pow_sign - 1) != ')'
+            || !isdigit(*(pow_sign - 2))
+            || !(div_sign = find_div_sign(pow_sign - 2, s))) {
+            p = pow_sign + 1;
+            continue;
+        }
+        memset(exp, '\0', STR_MAXLEN);
+        char* exp_end = extract_exp(exp, pow_sign);
+        if (!*exp || !exp_end) {
+            p = pow_sign + 1;
+            continue;
+        }
+        exp_len = strlen(exp);
+        insert_str(s, div_sign, exp);
+        insert_str(s, div_sign + exp_len + 1, "(");
+        insert_str(s, pow_sign + exp_len, exp);
+    }
+}
