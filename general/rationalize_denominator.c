ba75c2a8443eb7347dee1e4c3daa62683a01f910
mikezangus <zangusm@gmail.com>
2024-12-07 23:59:06 -0500

    commit

--- general/rationalize_denominator.c
+++ general/rationalize_denominator.c
@@ -1,10 +1,8 @@
 #include <ctype.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include "../main.h"
 #include "../utils/utils.h"
-#include "../formatting/formatting.h"
 
 static void find_bounds(char** l_bound, char** r_bound, const char* div_sign)
 {
@@ -26,20 +24,29 @@ static void find_bounds(char** l_bound, char** r_bound, const char* div_sign)
     *r_bound = extract_num_fwd((char[]){0}, exp_div_sign + 1) + 1;
 }
 
+static char* find_mult_1_start(const char* start, const char* min)
+{
+    int scale = 0;
+    for (const char* p = start; p >= min; p--) {
+        balance_chars(*p, &scale, '(', ')');
+        if (scale == 0) {
+            return (char*)p;
+        }
+    }
+    return NULL;
+}
+
 void rationalize_denominator(char* s)
 {
-
     char* p = s;
     char* div_sign;
     char* l_bound, * r_bound;
     char den[STR_MAXLEN] = {0};
     char* p_den = den;
     size_t den_len;
-    while (*p) {
-        format_str(s);
-        if (!(div_sign = strchr(p, '/'))) {
-            return;
-        }        
+    printf("\n\n\nRationalizing denominator. Start:\n%s\n\n", s);
+    while (*p && (div_sign = strchr(p, '/'))) {
+        printf("Loop:\n%s\n\n", div_sign);      
         find_bounds(&l_bound, &r_bound, div_sign);
         if (!l_bound || !r_bound) {
             p = div_sign + 1;
@@ -52,13 +59,13 @@ void rationalize_denominator(char* s)
             p = div_sign + 1;
             continue;
         }
-        strcat(den, ")");
+        strcat(den, "))");
         den_len = strlen(den);
-        insert_str(s, l_bound - 1, den);
-        insert_str(s, r_bound + den_len + 1, den);
-        div_sign+=den_len;
-        insert_str(s, div_sign + 1, "(");
-        p = r_bound + den_len + 1;
-        
+        insert_str(s, find_mult_1_start(div_sign++ - 1, s), "(");
+        insert_str(s, l_bound, den);
+        insert_str(s, div_sign + den_len + 1, "(");
+        insert_str(s, r_bound += 3 + den_len + 1, den);
+        p = r_bound + den_len;
+        printf("Rationalized:\n%s\n", s);
     }
 }
