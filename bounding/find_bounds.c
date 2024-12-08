4865dc845a5aadbc1e75bdeeb497987ed38a3453
mikezangus <zangusm@gmail.com>
2024-12-08 13:28:56 -0500

    handling for both sides of equals sign

--- bounding/find_bounds.c
+++ bounding/find_bounds.c
@@ -1,16 +1,25 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include "bounding.h"
 #include "../main.h"
+
+static bool sequence(const char* start, const char* end,
+                     char** l_bound, char** r_bound)
+{
+    return find_bounds_by_parens(start, l_bound, r_bound)
+        || find_bounds_by_oprtr(start, end, l_bound, r_bound);
+}
 
 bool find_bounds(const char* start, char** l_bound, char** r_bound)
 {
     *l_bound = NULL;
     *r_bound = NULL;
-    if (!find_bounds_by_parens(start, l_bound, r_bound)
-        && !find_bounds_by_oprtr(start, l_bound, r_bound)) {
-        return false;
+    const char* eq_sign = strchr(start, '=');
+    if (!sequence(start, eq_sign, l_bound, r_bound)) {
+        if (!eq_sign) {
+            return false;
+        }
+        return sequence(eq_sign + 1, NULL, l_bound, r_bound);
     }
-    printf("\nBounds: %.2s ... %.2s\n", *l_bound, *r_bound - 1);
-    return true;
 }
