b64471f72e0fdaaa5389a0e6aa5379daaf3ac4d5
mikezangus <zangusm@gmail.com>
2024-12-10 21:26:17 -0500

    significantly simpler

--- utils/searches/find_adj_oprtr.c
+++ utils/searches/find_adj_oprtr.c
@@ -1,40 +1,35 @@
 #include <stdio.h>
 #include "../utils.h"
 
-static char* find_l(const char* s, const char* l_start)
+static char* find_l(const char* src, const char* l_bound)
 {
-    const char* p = (is_paren(*l_start) && l_start > s) ? --l_start : l_start;
-    for (; p > s && !is_paren(*p); p--) {
-        if (is_oprtr(*p)) {
-            break;
+    if (l_bound && l_bound > src) {
+        if (is_oprtr(*(--l_bound))) {
+            return (char*)l_bound;
+        }
+        if (*l_bound == '(' && l_bound > src && is_oprtr(*(--l_bound))) {
+            return (char*)l_bound;
         }
     }
-    return (get_paren_depth(p, l_start) == 0) ? (char*)p : NULL;
+    return NULL;
 }
 
-static char* find_r(const char* r_start)
+static char* find_r(const char* r_bound)
 {
-    const char* p = (is_paren(*r_start)) ? ++r_start : r_start;
-    for (; *p && !is_paren(*p); p++) {
-        if (is_oprtr(*p)) {
-            break;
+    if (r_bound && *(++r_bound)) {
+        if (is_oprtr(*(r_bound))) {
+            return (char*)r_bound;
+        }
+        if (*r_bound == ')' && *(++r_bound) && is_oprtr(*r_bound)) {
+            return (char*)r_bound;
         }
     }
-    return (get_paren_depth(r_start, p) == 0) ? (char*)p : NULL;
+    return NULL;
 }
 
-char* find_adj_oprtr(const char* s, const char* l_start, const char* r_start)
+char* find_adj_oprtr(const char* src, const char* l_bound, const char* r_bound)
 {
-    char* l = l_start ? find_l(s, l_start) : NULL;
-    char* r = r_start ? find_r(r_start) : NULL;
-    if (!l && !r) {
-        return NULL;
-    }
-    if (!l) {
-        return r;
-    }
-    if (!r) {
-        return l;
-    }
-    return (l_start - l) <= (r - r_start) ? l : r;
+    char* l = l_bound ? find_l(src, l_bound) : NULL;
+    char* r = r_bound ? find_r(r_bound) : NULL;
+    return l ? l : r;
 }
