f765b4c7718d2c913002a4a7e2f165940f8e1415
mikezangus <zangusm@gmail.com>
2024-12-11 23:14:55 -0500

    better negsign handling

--- utils/checks/is_num.c
+++ utils/checks/is_num.c
@@ -2,10 +2,10 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool is_num(const char* p, const char* src)
+bool is_num(const char* p, const char* min)
 {   
     return isdigit(*p)
-           || (*p == '.' && src && p > src && isdigit(*(p - 1)))
-           || (*p == '-' && src && p == src)
-           || (*p == '-' && src && p > src && *(p - 1) == '=');
+           || (*p == '.' && min && p > min && isdigit(*(p - 1)))
+           || (*p == '-' && min && p > min && !isdigit(*(p - 1)))
+           || (*p == '-' && min && p == min);
 }
