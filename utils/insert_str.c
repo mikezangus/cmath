f160c57681e1e3e45d628a87e496fc5c30fe5064
mikezangus <zangusm@gmail.com>
2024-12-11 22:49:24 -0500

    better error handling

--- utils/insert_str.c
+++ utils/insert_str.c
@@ -1,27 +1,26 @@
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-static bool is_pos_inside_dst(char* dst_str, size_t dst_len,
-                              const char* insert_pos)
+void insert_str(char* dst, char* insert_pos, const char* src)
 {
-    return insert_pos >= dst_str && insert_pos <= dst_str + dst_len;
-}
-
-void insert_str(char* dst_str, const char* src_str, const char* insert_pos)
-{
-    size_t dst_len = strlen(dst_str);
-    if (!is_pos_inside_dst(dst_str, dst_len, insert_pos)) {
+    if (!insert_pos || !*src) {
         fprintf(stderr,
-                "\n%s | Error: insert_pos not inside of dst_str\n"
-                "  dst_str: %p - %p\n"
-                "  insert_pos: %p\n",
-                __FILE__, dst_str, dst_str + dst_len, insert_pos);
-        return;
+                "\n%s\nError | NULL value(s) pased into insert_str()\n",
+                __FILE__);
+        exit(1);
     }
-    size_t src_len = strlen(src_str);
-    memmove((char*)insert_pos + src_len,
-            insert_pos,
-            dst_len - (insert_pos - dst_str) + 1);
-    memcpy((char*)insert_pos, src_str, src_len);
+    size_t dst_len = strlen(dst);
+    if (!(dst <= insert_pos && insert_pos <= dst + dst_len)) {
+        fprintf(stderr,
+                "\n%s\n"
+                "Error | Insert position outside of destination string\n"
+                "  insert: %p\n"
+                "  dst:    %p - %p\n",
+                __FILE__, insert_pos, dst, dst + dst_len);
+        exit(1);
+    }
+    size_t src_len = strlen(src);
+    memmove(insert_pos + src_len, insert_pos, dst_len - (insert_pos - dst) + 1);
+    memcpy(insert_pos, src, src_len);
 }
