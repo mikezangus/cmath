888763ba7b9dcdde52c183687a8c8b49c644651b
mikezangus <zangusm@gmail.com>
2024-12-12 22:27:11 -0500

    formatting

---
+++ formatting/format_str.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include "formatting.h"
+#include "../utils/utils.h"
+
+void format_str(char* s)
+{
+    while (remove_unneeded_parens(s)
+           | convert_neg_signs(s)
+           | remove_1_oprtns(s)
+           | insert_parens(s)
+           | move_frctn_neg_signs(s)) {
+        if (get_paren_depth(s, NULL)) {
+            fprintf(stderr,
+                    "\n%s\nError | Imbalanced parentheses:\n%s\n",
+                    __FILE__, s);
+            exit(1);
+        }
+    }
+}
--- formatting/formatting.h
+++ formatting/formatting.h
@@ -3,11 +3,9 @@
 #include "../main.h"
 #include "../utils/utils.h"
 
-void flip_frctn_divisor(char* oprtr, char* n, char* d);
-void format_frctn_neg_signs(char* s, char* div_sign, Bounds* b);
-
 bool convert_neg_signs(char* s);
 void format_str(char* s);
 bool insert_parens(char* s);
+bool move_frctn_neg_signs(char* s);
 bool remove_1_oprtns(char* s);
 bool remove_unneeded_parens(char* s);
